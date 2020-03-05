8638f7a3ee91a4c21a0773020bacb65b8c45ac5c
AndreyMeshkow <meshkow.andrei@gmail.com>
2020-03-05 21:29:44 +0300

    feat: add ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-practice-part2';
+
+  arr = [1, 1, 2, 3, 5, 8, 13];
+
+  objs = [
+    {title: 'Post1', author: 'Andrey', comments: [
+        {name: 'Max', text: 'lorem1'},
+        {name: 'Max', text: 'lorem2'},
+        {name: 'Max', text: 'lorem3'}
+      ]},
+    {title: 'Post2', author: 'Andrey 2', comments: [
+        {name: 'Max2', text: 'lorem1'},
+        {name: 'Max2', text: 'lorem2'},
+        {name: 'Max2', text: 'lorem3'}
+      ]},
+  ];
+
 }
