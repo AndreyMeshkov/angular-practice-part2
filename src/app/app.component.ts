c70273ad8a6ce201218c7200c194f01fc78669ff
AndreyMeshkow <meshkow.andrei@gmail.com>
2020-03-05 21:42:26 +0300

    feat: add pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  arr = [1, 1, 2, 3, 5, 8, 13];
-
-  objs = [
-    {title: 'Post1', author: 'Andrey', comments: [
-        {name: 'Max', text: 'lorem1'},
-        {name: 'Max', text: 'lorem2'},
-        {name: 'Max', text: 'lorem3'}
-      ]},
-    {title: 'Post2', author: 'Andrey 2', comments: [
-        {name: 'Max2', text: 'lorem1'},
-        {name: 'Max2', text: 'lorem2'},
-        {name: 'Max2', text: 'lorem3'}
-      ]},
-  ];
+  now = new Date();
 
 }
