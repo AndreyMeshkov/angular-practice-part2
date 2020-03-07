d2b3b7ab39f70e806509af952a6aaee341db7e5d
AndreyMeshkow <meshkow.andrei@gmail.com>
2020-03-07 21:50:36 +0300

    feat: update post and post-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 export interface Post {
   title: string;
   text: string;
-  is?: number;
+  id?: number;
 }
 
 @Component({
@@ -13,8 +13,13 @@ export interface Post {
 })
 export class AppComponent {
   posts: Post[] = [
-    {title:"Хочу выучить Angular компоненты", text: "Я все еще учу компоненты", id: 1},
-    {title:"Следующий блок", text: "Будет про директивы и еще про пайпы", id: 2}
+    {title: 'Хочу выучить Angular компоненты', text: 'Я все еще учу компоненты', id: 1},
+    {title: 'Следующий блок', text: 'Будет про директивы и еще про пайпы', id: 2}
 
   ];
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post);
+    // console.log('Post', post);
+  }
 }
