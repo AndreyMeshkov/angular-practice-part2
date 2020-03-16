3dbed103204702c1930d8d73b16f4885c03d292b
AndreyMeshkow <meshkow.andrei@gmail.com>
2020-03-16 23:21:17 +0300

    feat: add filter.pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import {Component} from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,12 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
+  posts: Post[] = [
+    {title: 'Beer', text: 'The best drink in the world'},
+    {title: 'Bread', text: 'The best food in the world'},
+    {title: 'JavaScript', text: 'The best language in the world'},
+  ];
+
+  search: '';
+  field = 'title';
 }
