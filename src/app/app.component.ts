1e173568398e9054e2264ce91bc69546ea7afc6c
AndreyMeshkow <meshkow.andrei@gmail.com>
2020-03-19 22:20:08 +0300

    feat: add pipe optimization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,20 @@ export interface Post {
 })
 export class AppComponent {
 
+  search: '';
+  field = 'title';
+
   posts: Post[] = [
     {title: 'Beer', text: 'The best drink in the world'},
     {title: 'Bread', text: 'The best food in the world'},
     {title: 'JavaScript', text: 'The best language in the world'},
   ];
 
-  search: '';
-  field = 'title';
+  addPost() {
+    this.posts.unshift({
+      title: 'Angular 9',
+      text: 'Practice Angular course'
+    });
+  }
+
 }
