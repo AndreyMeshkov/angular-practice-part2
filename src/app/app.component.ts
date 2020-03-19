99a265b73c53c1dab76771000140410386586718
AndreyMeshkow <meshkow.andrei@gmail.com>
2020-03-19 22:43:08 +0300

    feat: add pipe async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
 
 export interface Post {
   title: string;
@@ -10,22 +11,25 @@ export interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  search: '';
-  field = 'title';
+  p: Promise<string> = new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve('Promise Resolved');
+    }, 4000);
+  });
 
-  posts: Post[] = [
-    {title: 'Beer', text: 'The best drink in the world'},
-    {title: 'Bread', text: 'The best food in the world'},
-    {title: 'JavaScript', text: 'The best language in the world'},
-  ];
+  date$: Observable<Date> = new Observable( obs => {
+    setInterval(() => {
+      obs.next(new Date());
+    }, 1000);
+  });
 
-  addPost() {
-    this.posts.unshift({
-      title: 'Angular 9',
-      text: 'Practice Angular course'
-    });
+  date: Date;
+
+  ngOnInit(): void {
+    // this.date$.subscribe( date => {
+    //   this.date = date;
+    // });
   }
-
 }
